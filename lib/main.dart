be86ee88be11309dfb9ff086315dc79c48b0dc15
Rafael Eduardo Lima <rafael.santos@midway.com.br>
2020-09-20 22:27:32 -0300

    116. configurando o projeto - firebase e testando

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
+
+  Firestore.instance
+      .collection("col")
+      .document("doc")
+      .setData({"texto": "RAFA"});
 }
 
 class MyApp extends StatelessWidget {
